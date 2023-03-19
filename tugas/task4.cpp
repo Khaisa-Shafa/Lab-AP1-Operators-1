c4b8d799e3dbdc2ba259e5d601e90883b39cbe98
khaisa-shafa <khaisash09@gmail.com>
2023-03-19 19:30:51 +0700

    second commit

--- tugas/task4.cpp
+++ tugas/task4.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 
 int main() {
-    int angka;
+    int time,hours,min,sec;
+    
+    cout<<"input time in sec : ";
+    cin>>time;
 
+    hours= time / 3600;          
+    sec = time % 3600;   
+    min = sec / 60;     
+    sec %= 60;            
+
+    cout<<"HH:MM:SS"<<endl;
+    cout<<"0"<<hours<<" : "<<"0"<<min<<" : "<<"0"<<sec;
     return 0;
 }
